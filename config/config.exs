b79155fe450a17fa1af993fd5ef3636bfdd4124e
Geoffrey Lessel <geo@geoffreylessel.com>
2016-07-17 14:34:31 -0700

    Configure repo for use
    Then run
    
    ```
    > mix ecto.create
    ```

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,14 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :todo, Todo.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "meetup_todo_repo",
+  username: "postgres",
+  password: "",
+  hostname: "localhost"
+
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
--- lib/todo.ex
+++ lib/todo.ex
@@ -10,6 +10,7 @@ defmodule Todo do
     children = [
       # Starts a worker by calling: Todo.Worker.start_link(arg1, arg2, arg3)
       # worker(Todo.Worker, [arg1, arg2, arg3]),
+      supervisor(Todo.Repo, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
